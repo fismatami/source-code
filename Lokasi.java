edb4a63944201643477a2a123de5b1eaecd1367b
melatisp <melaties.pe@gmail.com>
2016-03-18 11:32:45 +0700

    Update Lokasi.java

--- Lokasi.java
+++ Lokasi.java
@@ -1,46 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kelompok7;
-import kelompok7.Pembimbing;
 
-/**
- *
- * @author udev
- */
-import java.util.Scanner;
-import java.lang.Math;
+import java.util.ArrayList;
 
 public class Lokasi 
 {
-	private Kelompok[] k=new Kelompok[10];
-        private int jumlahKelompok=0;
+        private String lokasi;
+        private Pembimbing pembimbing;
+        private int nokelp=0;
+        private int maxkel=10;
+	private ArrayList<Kelompok> k=new ArrayList<Kelompok>();
+
+        public Lokasi(String lokasi, Pembimbing pemb){
+            this.lokasi=lokasi;
+            this.pembimbing=pemb;
+        }
+
+        public Pembimbing getPembimbing() {
+        return pembimbing;
+        }
+
+        public void setPembimbing(Pembimbing pembimbing) {
+        this.pembimbing = pembimbing;
+        }
+   
+        public String getLokasi() {
+        return lokasi;
+    
+        }
+
+        public int getnokelp() {
+        return nokelp;
+        }
 	
 	
-	public void createKelompok(int noKelp, String jurusan)
+	public void createKelompok(String jurusan)
 	{
-            if(jumlahKelompok<10){
-                k[jumlahKelompok]=new Kelompok(noKelp, jurusan);
-                jumlahKelompok++;
-            }
+            Kelompok kelp = new Kelompok(this.nokelp, jurusan, this.pembimbing);
+            k.add(kelp);
         }
         
-        public Kelompok getKelompok(int i){
-            return k[i];
+        public Kelompok searchKelompok(int noKelp){
+            Kelompok kelp =null;
+            noKelp--;
+            kelp=k.get(noKelp);
+            return kelp;
+        }
+        
+        public void addMahasiswaToKelompok(int noKelp, Mahasiswa m){
+            Kelompok search = searchKelompok(noKelp);
+            search.addAnggota(m);
+            System.out.println(m.getNama()+" Telah Ditambahkan dalam kelompok "+noKelp);
         }
         
         public void displayInfo() {
 		System.out.println("DATA KELOMPOK");
                 System.out.println();
-                for(int i=1; i<10; i++){
-                    System.out.println("Kelompok "+"\n[["+getKelompok(i)+"]");
-                    System.out.println("Pembimbing\t:"+k[i].getPembimbing().getNama());
-                    for(int j=0; j<10; j++){
-                        System.out.println("\tAnggota "+(j+1));
-                        System.out.println("\t");
-                    }
+                for(int i=0; i<k.size(); i++){
+                    System.out.println("Kelompok\t:"+(k.get(i).getNoKelp()+1));
+                    System.out.println("Jurusan \t:"+k.get(i).getJurusan());
+                    System.out.println("Pembimbing : "+k.get(i).getPembimbing().getNama());
+                    System.out.println("Anggota : ");
+                    k.get(i).displayKelompok();
                 }
                 
 	}
